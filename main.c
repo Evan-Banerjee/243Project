ee184d723db6a7052b789e9f5aa381ba070129ef
Evan Banerjee <ebanerjee22@gmail.com>
2024-03-26 16:07:53 -0400

    setting up some strcuts

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct {
+    int xCoordinate;
+    int yCoordinate;
+    int zCoordinate;
+} ThreeDimensionalPoint;
+
+typedef struct {
+    int xCoordinate;
+    int yCoordinate;
+} TwoDimensionalPoint;
+
+
+int main(){
+    printf("hello world this is evan");
+    return 0;
+}
